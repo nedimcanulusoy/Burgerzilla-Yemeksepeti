74b330ffe96d41e028674addefa7faef1e441f10
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-02-03 23:44:06 +0100

    ADD __init__.py

---
+++ burgerzilla/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_restx import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+api = Api(app, doc="/docs", title="Burgerzilla API", description="Burgerzilla Yemeksepeti API", version="1.0")
+
+from burgerzilla import routes, models
