d4bd49786572ea83a6fb8b5bc7cbe3ec5804243f
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-02-03 23:43:36 +0100

    ADD config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os, yaml
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    stream = open(os.getcwd() + "/config.yml", 'r')
+    config = yaml.load(stream, Loader=yaml.FullLoader)
+
+    SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{config["database"]["user"]}:' \
+                           f'{config["database"]["password"]}' \
+                           f'@{config["database"]["host"]}/{config["database"]["db"]}'
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
