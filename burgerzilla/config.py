55d287ba29674ea76681e0c2e06446f76202a864
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-02-09 00:00:45 +0100

    UPDATE relocate config.py

--- burgerzilla/config.py
+++ burgerzilla/config.py
@@ -7,6 +7,7 @@ class Config(object):
     stream = open(os.getcwd() + "/config.yml", 'r')
     config = yaml.load(stream, Loader=yaml.FullLoader)
 
+    SECRET_KEY = config["secret"]
     SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{config["database"]["user"]}:' \
                            f'{config["database"]["password"]}' \
                            f'@{config["database"]["host"]}/{config["database"]["db"]}'
