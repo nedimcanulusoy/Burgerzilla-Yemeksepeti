839081389f6054b7320767620de368eb747947f5
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-02-09 00:00:19 +0100

    UPDATE api_models.py

--- burgerzilla/api_models.py
+++ burgerzilla/api_models.py
@@ -11,22 +11,49 @@ User_Dataset = api.model('User', {'name': fields.String(),
 
 Restaurant_Dataset = api.model('Restaurant', {'name': fields.String()})
 
-Menu_Dataset = api.model('Menu', {'product': fields.String(),
-                                  'price': fields.String(),
+Menu_Dataset = api.model('Menu', {'name': fields.String(),
+                                  'price': fields.Integer(),
                                   'description': fields.String(),
                                   'image': fields.String(),
                                   'restaurant_id': fields.Integer()})
 
-Order_Dataset = api.model('Order', {'name': fields.String(),
-                                    'price': fields.Integer(),
-                                    'quantity': fields.Integer(),
-                                    'status': fields.String(),
+Order_Detail_Dataset = api.model('OrderDetail', {'name': fields.String(),
+                                                 'address': fields.String(),
+                                                 'status': fields.String(default='NEW'),
+                                                 'timestamp': fields.String(),
+                                                 'restaurant_id': fields.Integer(),
+                                                 'user_id': fields.Integer(),
+                                                 'menus': fields.List(fields.Nested(Menu_Dataset)),
+                                                 'sum_price': fields.Integer()
+                                                 })
+
+Order_Dataset = api.model('Order', {'id': fields.Integer(),
+                                    'status': fields.String(default='NEW'),
                                     'timestamp': fields.String(),
                                     'restaurant_id': fields.Integer(),
-                                    'user_id': fields.Integer()
+                                    'user_id': fields.Integer(),
                                     })
+
+# New order data set
+New_Order_Dataset = api.model('Order', {'status': fields.String(default='NEW'),
+                                        'restaurant_id': fields.Integer(),
+                                        'user_id': fields.Integer()})
 
 Order_Menu_Dataset = api.model('Order_Menu', {'order_id': fields.Integer(),
                                               'menu_id': fields.Integer()})
 
 Order_Menu_ID_Dataset = api.model('Order_Menu', {'menu_id': fields.Integer()})
+
+Restaurant_Order_Dataset = api.model('Order', {'name': fields.String(),
+                                               'address': fields.String(),
+                                               'status': fields.String(),
+                                               'timestamp': fields.String(),
+                                               'restaurant_id': fields.Integer(),
+                                               'user_id': fields.Integer(),
+                                               'menus': fields.List(fields.Nested(Menu_Dataset)),
+                                               'sum_price': fields.Integer()
+                                               })
+
+Delete_Msg = {'Message': fields.String(default="Order has been successfully deleted!")}
+
+Error_Dataset = api.model("Error", {"result": "error", "message": fields.String})
