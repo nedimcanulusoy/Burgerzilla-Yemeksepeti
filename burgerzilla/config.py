7081e6ad29578ebfaebbf4cc25ea86bfe1126e92
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-02-10 20:11:27 +0100

    ADD creating a config.yml file from scratch

--- burgerzilla/config.py
+++ burgerzilla/config.py
@@ -1,13 +1,35 @@
 import os, yaml
+from os.path import exists
+import secrets
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config(object):
-    stream = open(os.getcwd() + "/config.yml", 'r')
+    config_path = os.getcwd() + "/config.yml"
+    file_exists = exists(config_path)
+
+    if not file_exists:
+        data = dict(
+            secret=secrets.token_hex(32),
+
+            database=dict(
+                user='fill-here-with-your-data',
+                db='fill-here-with-your-data',
+                password='fill-here-with-your-data',
+                host='fill-here-with-your-data',
+                port='fill-here-with-your-data',
+            )
+        )
+
+        with open(config_path, 'w') as outfile:
+            yaml.dump(data, outfile, default_flow_style=False)
+
+
+    stream = open(config_path, 'r')
     config = yaml.load(stream, Loader=yaml.FullLoader)
 
     SECRET_KEY = config["secret"]
+    JWT_SECRET_KEY = config["secret"]
     SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{config["database"]["user"]}:' \
                            f'{config["database"]["password"]}' \
                            f'@{config["database"]["host"]}/{config["database"]["db"]}'
