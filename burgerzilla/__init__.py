e88b15b43a001215e7de199b035280cc3a4d2869
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-02-10 20:15:57 +0100

    ADD login and register routes

--- burgerzilla/__init__.py
+++ burgerzilla/__init__.py
@@ -8,7 +8,10 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+jwt = JWTManager(app)
 api = Api(app, doc="/docs", title="Burgerzilla API", description="Burgerzilla Yemeksepeti API", version="1.0")
 
 from burgerzilla.customer import customer
 from burgerzilla.restaurant import restaurant
+from burgerzilla.auth.register import register
+from burgerzilla.auth.login import login
