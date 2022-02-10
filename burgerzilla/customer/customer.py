bccd06c27b728981fb8214455aff92999347b20e
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-02-10 20:09:01 +0100

    ADD JWT feature

--- burgerzilla/customer/customer.py
+++ burgerzilla/customer/customer.py
@@ -1,41 +1,19 @@
 from flask import request
 from burgerzilla import api, db
 from flask_restx import Resource
-from burgerzilla.api_models import (User_Dataset, Order_Dataset, Order_Menu_Dataset, Order_Menu_ID_Dataset,
+from burgerzilla.api_models import (Order_Dataset, Order_Menu_Dataset, Order_Menu_ID_Dataset,
                                     New_Order_Dataset, Order_Detail_Dataset, Response_Message)
 from burgerzilla.models import User, Menu, Order, Order_Menu
-
-
-@api.route('/user')
-class CustomerOperations(Resource):
-    @api.marshal_list_with(User_Dataset, code=200, envelope='users')
-    def get(self):
-        all_customers = User.query.all()
-        return all_customers
-
-    @api.marshal_with(Response_Message, code=201, envelope='user')
-    def post(self):
-        json_data = request.get_json()
-        name = json_data.get('name')
-        surname = json_data.get('surname')
-        username = json_data.get('username')
-        email = json_data.get('email')
-        password = json_data.get('password')
-        address = json_data.get('address')
-        restaurant_id = json_data.get('restaurant_id')
-        new_user = User(name=name, surname=surname,
-                        username=username, email=email,
-                        password=password, address=address, restaurant_id=restaurant_id)
-        db.session.add(new_user)
-        db.session.commit()
-        return {"Message": "Successful"}, 200
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 
 @api.route('/customer/order')
 class OrderOperations(Resource):
-    @api.marshal_with(Order_Detail_Dataset,Response_Message)
+    @jwt_required()
+    @api.response(model=Order_Detail_Dataset, code=201, description="Successful")
+    @api.response(model=Response_Message, code=404, description="Not found")
     def get(self):
-        user_id = 1  # JWT den gelmis gibi sayiliyor
+        user_id = get_jwt_identity()  # JWT den gelmis gibi sayiliyor
         order = Order.query.filter_by(status='NEW', user_id=user_id).first()
         if order == None:
             return {"Message": "There is no valid order!"}, 404
@@ -56,29 +34,12 @@ class OrderOperations(Resource):
             return {"name": user.name, 'address': user.address, 'timestamp': order.timestamp, 'user_id': user_id,
                     'restaurant_id': order.restaurant_id, "menus": item_list, "sum_price": price}, 200
 
-    @api.marshal_with(New_Order_Dataset,Response_Message, envelope='order')
-    def post(self):
-        user_id = 1
-        user = User.query.get(user_id)  # JWT'den geliyor
-        order = Order.query.get(user_id)
-        status = "NEW"  # statik tanımlanmış durumda
-
-        if order.status == status:
-            return {"Message": "You can not create another one until your current order is complete!"}, 404
-        else:
-            new_order = Order(status=status, restaurant_id=user.restaurant_id,
-                              user_id=user_id)
-
-            db.session.add(new_order)
-            db.session.commit()
-            return new_order, 201  # New order data set
-
 
 @api.route('/customer/orders')
 class ListOrders(Resource):
     @api.marshal_list_with(Order_Dataset, code=200)
     def get(self):
-        user_id = 1
+        user_id = get_jwt_identity()
         # user = User.query.get(user_id) # JWT'den geliyor
         orders = Order.query.filter_by(user_id=user_id)
         ordersList = []
@@ -91,9 +52,10 @@ class ListOrders(Resource):
 
 @api.route('/customer/order/add')
 class OrderAdd(Resource):
+    @jwt_required()
     @api.marshal_with(Order_Menu, code=200, envelope='update_order')
     def post(self):
-        user_id = 1
+        user_id = get_jwt_identity()
         menu_id = 1
         user = User.query.get(user_id)  # JWT'den geliyor
         return user.id
@@ -101,10 +63,11 @@ class OrderAdd(Resource):
 
 @api.route('/customer/order/delete')
 class OrderDelete(Resource):
+    @jwt_required()
     @api.marshal_with(Response_Message)
     def post(self):
-        user_id = 1 #JWT den gelecek
-        menu_id = 1 #Postmandan verilecek
+        user_id = get_jwt_identity()
+        menu_id = 1  # Postmandan verilecek
         menu_id_exists = db.session.query(Menu.id).first() is not None
 
         if not menu_id_exists:
@@ -112,7 +75,8 @@ class OrderDelete(Resource):
 
         order_id = db.session.query(Order).filter_by(user_id=user_id, status="NEW").first().id
 
-        menu_exists_in_order_menu_table = db.session.query(Order_Menu).filter_by(order_id=order_id, menu_id=menu_id).first() is not None
+        menu_exists_in_order_menu_table = db.session.query(Order_Menu).filter_by(order_id=order_id,
+                                                                                 menu_id=menu_id).first() is not None
 
         if not menu_exists_in_order_menu_table:
             return {"Message": "Order could not deleted, because there is no valid menu!"}, 404
@@ -121,15 +85,17 @@ class OrderDelete(Resource):
         db.session.delete(del_order)
         db.session.commit()
 
-
         return {"Message": "Order successfully deleted!"}, 200
+
 
 @api.route('/customer/order/cancel')
 class OrderCancel(Resource):
+    @jwt_required()
     @api.marshal_with(Response_Message)
     def post(self):
-        user_id = 1
-        order = db.session.query(Order).filter(Order.user_id==user_id, Order.status=="NEW").first()  # kullancinin siparisi var mi (sepet/order)
+        user_id = get_jwt_identity()
+        order = db.session.query(Order).filter(Order.user_id == user_id,
+                                               Order.status == "NEW").first()  # kullancinin siparisi var mi (sepet/order)
         order_exists = order is not None  # kullancinin siparisi var mi (sepet/order)
 
         if not order_exists:
@@ -143,23 +109,31 @@ class OrderCancel(Resource):
 
         return {"Message": "Your order has been cancelled!"}, 200
 
-@api.route('/customer/order/<int:id>')
+
+@api.route('/customer/order/menu')
 class OrderMenuOperations(Resource):
+    @jwt_required()
     @api.marshal_list_with(Order_Menu_ID_Dataset, code=200, envelope='order_menu')
-    def get(self, id):
+    def get(self):
         '''Returns how many menus have been ordered by the user'''
-        menus = Order_Menu.query.filter_by(order_id=id)
+        user_id = 1
+        menus = Order_Menu.query.filter_by(order_id=user_id)
         menu_list = []
         for each in menus:
             menu_list.append(each)
 
         return menu_list
 
+    @jwt_required()
     @api.marshal_with(Order_Menu_Dataset, code=201, envelope='order_menu')
-    def post(self, id):
+    def post(self):
+        user_id = get_jwt_identity()
         json_data = request.get_json()
         menu_id = json_data.get('menu_id')
-        new_body = Order_Menu(menu_id=menu_id, order_id=id)
+
+        user_order_id = User.query.filter_by(order_id=user_id).first()
+
+        new_body = Order_Menu(menu_id=menu_id, order_id=user_order_id)
         db.session.add(new_body)
         db.session.commit()
         return new_body
