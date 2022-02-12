9a675a77853eb9fd69d1968b5fe4d2d9cffeb520
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-02-13 00:27:13 +0100

    ADD auth namespace as auth_ns

--- burgerzilla/routes/__init__.py
+++ burgerzilla/routes/__init__.py
@@ -1,7 +1,8 @@
 from flask_restx import Namespace
 
+auth_ns = Namespace("auth", description="Auth Operations")
 customer_ns = Namespace("customer", description="Customer Operations")
 restaurant_ns = Namespace("restaurant", description="Restaurant Operations")
 menu_ns = Namespace("menus", description="Menu Operation")
 
-from burgerzilla.routes import customer, restaurant
+from burgerzilla.routes import customer, restaurant, auth, menus
