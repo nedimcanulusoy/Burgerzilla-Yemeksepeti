3b8dc9a43c31df1df109adca466dc9c454f4077b
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-02-13 00:17:22 +0100

    UPDATE reformat

--- burgerzilla/api_models.py
+++ burgerzilla/api_models.py
@@ -1,34 +1,44 @@
 from flask_restx import fields
 from burgerzilla import api
+from burgerzilla.routes import customer_ns, restaurant_ns, auth_ns
 
-User_Dataset = api.model('User', {'name': fields.String(),
-                                  'surname': fields.String(),
-                                  'username': fields.String(),
-                                  'email': fields.String(),
-                                  'password': fields.String(),
-                                  'address': fields.String(),
-                                  'restaurant_id': fields.Integer(default=-1),
-                                  'is_owner': fields.Boolean(default=False),
-                                  'restaurant': fields.String,
-                                  'Message':fields.String})
+Restaurant_Dataset = restaurant_ns.model('Restaurant', {
+    'name': fields.String()
+})
 
-Restaurant_Dataset = api.model('Restaurant', {'name': fields.String()})
+User_Dataset = customer_ns.model('User', {
+    'name': fields.String(),
+    'surname': fields.String(),
+    'username': fields.String(),
+    'email': fields.String(),
+    'password': fields.String(),
+    'address': fields.String(),
+    'is_owner': fields.Boolean(default=False),
+    'restaurant': fields.Nested(Restaurant_Dataset)
+})
 
-Menu_Dataset = api.model('Menu', {'name': fields.String(),
-                                  'price': fields.Integer(),
-                                  'description': fields.String(),
-                                  'image': fields.String(),
-                                  'restaurant_id': fields.Integer()})
+Restaurant_ID_Dataset = restaurant_ns.model('Restaurant ID', {
+    'restaurant_id': fields.Integer()
+})
 
-Order_Detail_Dataset = api.model('OrderDetail', {'name': fields.String(),
-                                                 'address': fields.String(),
-                                                 'status': fields.String(default='NEW'),
-                                                 'timestamp': fields.String(),
-                                                 'restaurant_id': fields.Integer(),
-                                                 'user_id': fields.Integer(),
-                                                 'menus': fields.List(fields.Nested(Menu_Dataset)),
-                                                 'sum_price': fields.Integer()
-                                                 })
+Menu_Dataset = restaurant_ns.model('Menu', {
+    'name': fields.String(),
+    'price': fields.Integer(),
+    'description': fields.String(),
+    'image': fields.String(),
+    'restaurant_id': fields.Integer()
+})
+
+Order_Detail_Dataset = customer_ns.model('OrderDetail', {
+    'name': fields.String(),
+    'address': fields.String(),
+    'status': fields.String(default='NEW'),
+    'timestamp': fields.String(),
+    'restaurant_id': fields.Integer(),
+    'user_id': fields.Integer(),
+    'menus': fields.List(fields.Nested(Menu_Dataset)),
+    'sum_price': fields.Integer()
+})
 
 Order_Dataset = api.model('Order', {'id': fields.Integer(),
                                     'status': fields.String(default='NEW'),
@@ -38,32 +48,47 @@ Order_Dataset = api.model('Order', {'id': fields.Integer(),
                                     })
 
 # New order data set
-New_Order_Dataset = api.model('Order', {'status': fields.String(default='NEW'),
-                                        'restaurant_id': fields.Integer(),
-                                        'user_id': fields.Integer()})
+New_Order_Dataset = customer_ns.model('Order', {
+    'status': fields.String(default='NEW'),
+    'restaurant_id': fields.Integer(),
+    'user_id': fields.Integer()
+})
 
-Order_Menu_Dataset = api.model('Order_Menu', {'order_id': fields.Integer(),
-                                              'menu_id': fields.Integer()})
+Order_ID_Dataset = customer_ns.model('Order ID', {
+    'order_id': fields.Integer()
+})
 
-Order_Menu_ID_Dataset = api.model('Order_Menu', {'menu_id': fields.Integer()})
+Order_Menu_Dataset = restaurant_ns.model('Order_Menu', {
+    'order_id': fields.Integer(),
+    'menu_id': fields.Integer()
+})
 
-Restaurant_Order_Dataset = api.model('Order', {'name': fields.String(),
-                                               'address': fields.String(),
-                                               'status': fields.String(),
-                                               'timestamp': fields.String(),
-                                               'restaurant_id': fields.Integer(),
-                                               'user_id': fields.Integer(),
-                                               'menus': fields.List(fields.Nested(Menu_Dataset)),
-                                               'sum_price': fields.Integer()
-                                               })
+Order_Menu_ID_Dataset = customer_ns.model('Order_Menu_ID', {
+    'menu_id': fields.Integer()
+})
+
+Restaurant_Order_Dataset = restaurant_ns.model('Order', {
+    'name': fields.String(),
+    'address': fields.String(),
+    'status': fields.String(),
+    'timestamp': fields.String(),
+    'restaurant_id': fields.Integer(),
+    'user_id': fields.Integer(),
+    'menus': fields.List(fields.Nested(Menu_Dataset)),
+    'sum_price': fields.Integer()
+})
 
 Response_Message = api.model('Message', {'Message': fields.String()})
 
 Error_Dataset = api.model("Error", {"result": "error", "message": fields.String})
 
-Login_Dataset = api.model('User', {'username': fields.String(),
-                                  'password': fields.String()})
+Login_Dataset = auth_ns.model('Login User', {
+    'username': fields.String(),
+    'password': fields.String()
+})
 
-JWT_Dataset = api.model('JWT_Dataset', {'Message':fields.String,
-                                        'access_token':fields.String,
-                                        'refresh_token':fields.String})
+JWT_Dataset = api.model('JWT_Dataset', {
+    'Message': fields.String(),
+    'access_token': fields.String(),
+    'refresh_token': fields.String()
+})
