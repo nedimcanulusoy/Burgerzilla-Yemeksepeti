f0911536bc340f00b1ffbc118f439eb589721c33
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-02-13 14:15:56 +0100

    ADD restaurants namespace as restaurants_ns

--- burgerzilla/routes/__init__.py
+++ burgerzilla/routes/__init__.py
@@ -3,6 +3,7 @@ from flask_restx import Namespace
 auth_ns = Namespace("auth", description="Auth Operations")
 customer_ns = Namespace("customer", description="Customer Operations")
 restaurant_ns = Namespace("restaurant", description="Restaurant Operations")
-menu_ns = Namespace("menus", description="Menu Operation")
+menu_ns = Namespace("menus", description="Getting all menus from any restaurant")
+restaurants_ns = Namespace("restaurants", description="Getting all restaurants")
 
-from burgerzilla.routes import customer, restaurant, auth, menus
+from burgerzilla.routes import customer, restaurant, auth, menus, restaurants
